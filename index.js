35bef915497675872ba1d272c0fef88780f699d2
Evin J Manuel <evinjmanuel15@gmail.com>
2020-07-20 20:19:33 +0530

    fixed a error

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
 // app.use(express.static(__dirname + '/routes'));
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 
 const PORT = process.env.PORT || 3000    
 
 app.get("/",function(req,res){
-    req.logout();
-    res.render("index");
+    res.send("index");
 });
 
 server=http.listen(PORT,function(){
-    console.log("Runnning on 1690");
+    console.log("Runnning on 3000");
 });
