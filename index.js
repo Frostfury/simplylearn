a61fe2ebb62efdc8d42169152be3c246b42e9868
Evin J Manuel <evinjmanuel15@gmail.com>
2020-07-20 19:59:52 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express                 =   require('express'),
+    app                     =   express(),
+    http                    =   require('http').Server(app),
+    io                      =   require('socket.io')(http),
+    bodyParser              =   require("body-parser"),
+    moment                  =   require("moment");
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + '/routes'));
+app.use(methodOverride("_method"));
+
+const PORT = process.env.PORT || 3000    
+
+server=http.listen(PORT,function(){
+    console.log("Runnning on 1690");
+});
