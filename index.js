8fd4d1d059a74816dc46f62caa617c50533db7ae
Evin J Manuel <evinjmanuel15@gmail.com>
2020-07-20 20:02:36 +0530

    changes to previous commit

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ var express                 =   require('express'),
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
-app.use(express.static(__dirname + '/routes'));
+// app.use(express.static(__dirname + '/routes'));
 app.use(methodOverride("_method"));
 
 const PORT = process.env.PORT || 3000    
+
+app.get("/",function(req,res){
+    req.logout();
+    res.render("index");
+});
 
 server=http.listen(PORT,function(){
     console.log("Runnning on 1690");
